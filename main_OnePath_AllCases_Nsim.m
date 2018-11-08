b0f9e509474c73bc94787629ac29f49d454095de
venkata sasikiran veeramachaneni <vveeram2@gmu.edu>
2018-11-08 01:24:36 -0500

    This is the commit is mainly about the main_OnPath_AllCases_Nsim. In this i added the code for calculating ping performance and have DataStore. It is similar to the done in UKF one. Refer to the commit 61cad4d of UKFPDAF-predictAhead2 branch PingPerformance if you want to learn more

--- main_OnePath_AllCases_Nsim.m
+++ main_OnePath_AllCases_Nsim.m
@@ -5,11 +5,13 @@ for clutterLoop = 1:length(clutterVersion)
     yawDevRateLoopMin = 4.5;
     yawDevRateLoopJump = .5;
     yawDevRateLoopMax = 7;
+    maxPings = 150;
     nYaw = (yawDevRateLoopMax/yawDevRateLoopJump)+1;
     nYawStart = yawDevRateLoopMin/yawDevRateLoopJump;
     if clutterLoop == 1
         delete performance.mat
-        delete loopParam.mat                   
+        delete loopParam.mat
+        delete DataStore.mat
         IMM_TP.C = zeros(length(clutterVersion),nYaw-nYawStart);
         IMM_RMS.C = zeros(length(clutterVersion),nYaw-nYawStart);
         IMM_TP.F = zeros(length(clutterVersion),nYaw-nYawStart);
@@ -17,11 +19,19 @@ for clutterLoop = 1:length(clutterVersion)
         IMM_TP.I = zeros(length(clutterVersion),nYaw-nYawStart);
         IMM_RMS.I = zeros(length(clutterVersion),nYaw-nYawStart);
         IMM_TP.P = zeros(length(clutterVersion),nYaw-nYawStart);
-        IMM_RMS.P = zeros(length(clutterVersion),nYaw-nYawStart);        
+        IMM_RMS.P = zeros(length(clutterVersion),nYaw-nYawStart);
+        
+       IMM_Ping.C = zeros(maxPings,nYaw-nYawStart);
+       IMM_Ping.F = zeros(maxPings,nYaw-nYawStart);
+       IMM_Ping.I = zeros(maxPings,nYaw-nYawStart);
+       IMM_Ping.P = zeros(maxPings,nYaw-nYawStart);
         save('performance','IMM_TP','IMM_RMS');        
     end
     for yawDevRateLoop = yawDevRateLoopMin:yawDevRateLoopJump:yawDevRateLoopMax
-    totalSim = 1000;        
+    totalSim = 3;
+    if yawDevRateLoop == yawDevRateLoopMin
+        DataStore = Init_Data_Store(totalSim,nYaw-nYawStart);
+    end
     tempTP.C = zeros(totalSim,1);
     tempRMS.C = zeros(totalSim,1);    
     tempTP.F = zeros(totalSim,1);
@@ -30,6 +40,11 @@ for clutterLoop = 1:length(clutterVersion)
     tempRMS.I = zeros(totalSim,1);
     tempTP.P = zeros(totalSim,1);
     tempRMS.P = zeros(totalSim,1);
+    
+    tempPing.C = zeros(maxPings,1);
+    tempPing.F = zeros(maxPings,1);
+    tempPing.I = zeros(maxPings,1);    
+    tempPing.P = zeros(maxPings,1);    
     
         for nSim = 1:totalSim
             if nSim == 1 && yawDevRateLoop == yawDevRateLoopMin && clutterLoop == 1
@@ -41,7 +56,7 @@ for clutterLoop = 1:length(clutterVersion)
     %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Struct_Param_Path_Init(turnState,turnRate,yawDev,targetStartPoint,tLimit,vicinityLimit)
 %             ParPath = Struct_Param_Path_Init(true,yawDevRateLoop,90,[6000,4000],400,100);
-            ParPath = Struct_Param_Path_Init(true,yawDevRateLoop,90,[6000,4000],400,100);
+            ParPath = Struct_Param_Path_Init(true,yawDevRateLoop,90,[6000,4000],2*maxPings,100);
             ParGen = Struct_Param_Gen_Init(1/clutterVersion(clutterLoop));% clutterdensity inside the parenthesis
 
     %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -76,25 +91,44 @@ for clutterLoop = 1:length(clutterVersion)
                 FmIndx = find(transWave.estIMM == "F");
                 trackPoints = sum(diffBtwTrueExp(CwIndx) <= Threshold.cw) + sum(diffBtwTrueExp(FmIndx) <= Threshold.fm);
                 trackCommInd = [FmIndx(diffBtwTrueExp(FmIndx) <= Threshold.fm);CwIndx(diffBtwTrueExp(CwIndx) <= Threshold.cw)];
-                          
+                
+                dummyTrackPings = zeros(maxPings,1);
+                dummyTrackPings(trackCommInd) = 1;
                 switch caseLoop
                     case "C" 
                         tempTP.C(nSim) = (trackPoints/impLength)*100;
                         tempRMS.C(nSim) = sqrt(sum(diffBtwTrueExp(trackCommInd).^2)...
-                                                /length(trackCommInd)); 
+                                                /length(trackCommInd));
+                                            
+                        tempPing.C = tempPing.C + dummyTrackPings;
+                        DataStore((yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart,nSim).CW.Pings = trackCommInd;
+                        DataStore((yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart,nSim).CW.maxPing = impLength;
                     case "F"
                         tempTP.F(nSim) = (trackPoints/impLength)*100;
                         tempRMS.F(nSim) = sqrt(sum(diffBtwTrueExp(trackCommInd).^2)...
                                                 /length(trackCommInd));
+                                            
+                        tempPing.F = tempPing.F + dummyTrackPings;
+                        DataStore((yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart,nSim).FM.Pings = trackCommInd;
+                        DataStore((yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart,nSim).FM.maxPing = impLength;
                     case "I"
                         tempTP.I(nSim) = (trackPoints/impLength)*100;
                         tempRMS.I(nSim) = sqrt(sum(diffBtwTrueExp(trackCommInd).^2)...
                                                 /length(trackCommInd));
+                                            
+                        tempPing.I = tempPing.I + dummyTrackPings;
+                        DataStore((yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart,nSim).I.Pings = trackCommInd;
+                        DataStore((yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart,nSim).I.maxPing = impLength;
                     case "P"
                         tempTP.P(nSim) = (trackPoints/(impLength))*100;
                         tempRMS.P(nSim) = sqrt(sum(diffBtwTrueExp(trackCommInd).^2)...
                                                 /length(trackCommInd));
+                                            
+                        tempPing.P = tempPing.P + dummyTrackPings;
+                        DataStore((yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart,nSim).P.Pings = trackCommInd;
+                        DataStore((yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart,nSim).P.maxPing = impLength;
                 end
+            end
                 if nSim == totalSim
                     load('performance');
                     IMM_TP.C(clutterLoop,(yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart) = mean(tempTP.C);
@@ -107,23 +141,41 @@ for clutterLoop = 1:length(clutterVersion)
                     IMM_RMS.P(clutterLoop,(yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart) = mean(tempRMS.P);
                     tempVal(clutterLoop,(yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart).TP = tempTP;
                     tempVal(clutterLoop,(yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart).RMS = tempRMS;
+                    
+                    IMM_Ping.C(:,(yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart) = (tempPing.C ./ totalSim)*100;
+                    IMM_Ping.F(:,(yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart) = (tempPing.F ./ totalSim)*100;
+                    IMM_Ping.I(:,(yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart) = (tempPing.I ./ totalSim)*100;
+                    IMM_Ping.P(:,(yawDevRateLoop/yawDevRateLoopJump)+1 - nYawStart) = (tempPing.P ./ totalSim)*100;
                 end
-
-    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-                
-            end
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             save('loopParam','clutterVersion','totalSim');
             if nSim == totalSim
-                save('performance','IMM_TP','IMM_RMS','tempVal');
+                save('performance','IMM_TP','IMM_RMS','IMM_Ping','tempVal');
                 clearvars -except clutterLoop yawDevRateLoop yawDevRateLoopMin ...
-                yawDevRateLoopJump nYawStart;
+                yawDevRateLoopJump nYawStart maxPings DataStore;
             else
                 clearvars -except clutterLoop yawDevRateLoop yawDevRateLoopMin ...
-                yawDevRateLoopJump nYawStart tempTP tempRMS;
+                yawDevRateLoopJump nYawStart tempTP tempPing tempRMS maxPings DataStore;
             end 
         end
         disp(['yawDevRateLoop:', num2str(yawDevRateLoop)]);
     end
+    save('DataStore','DataStore')
     disp(['cluttterLoop:',num2str(clutterLoop)]);    
     clear;
+end
+
+function DataStore = Init_Data_Store(totalSim,length)
+%     for i = 1:length
+        DataStore(length,totalSim).CW.Pings = 0;
+        DataStore(length,totalSim).CW.maxPing = 0;
+        
+        DataStore(length,totalSim).FM.Pings = 0;
+        DataStore(length,totalSim).FM.maxPing = 0;
+        
+        DataStore(length,totalSim).I.Pings = 0;
+        DataStore(length,totalSim).I.maxPing = 0;
+        
+        DataStore(length,totalSim).P.Pings = 0;
+        DataStore(length,totalSim).P.maxPing = 0;
 end
